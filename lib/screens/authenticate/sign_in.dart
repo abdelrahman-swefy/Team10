d5fbc8e27ed6682c873953793165f4ad03ad3451
abdelrahman-swefy <62445954+abdelrahman-swefy@users.noreply.github.com>
2021-06-19 21:54:20 +0200

    Update sign_in.dart

--- lib/screens/authenticate/sign_in.dart
+++ lib/screens/authenticate/sign_in.dart
@@ -43,8 +43,8 @@ class _SignInState extends State<SignIn> {
                         padding: EdgeInsets.fromLTRB(30, 20, 30, 0),
                         child: TextFormField(
                           decoration: InputDecoration(
-                            hintText: "username",
-                            labelText: "username",
+                            hintText: "eg: xxx@gmail.com (no space after com)",
+                            labelText: "email",
                             border: OutlineInputBorder(
                                 borderRadius: BorderRadius.circular(15)),
                           ),
